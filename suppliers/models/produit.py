9f733044d1beaebc10894065c7b60ce709bb9b04
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-22 10:06:01 +0200

    ajouts des attributs de produit dans models

---
+++ suppliers/models/produit.py
@@ -0,0 +1,11 @@
+from django.db import models
+from suppliers.models import Fournisseur, CategorieProduit
+
+class Inscription(models.Model):
+    
+    nom = models.CharField()
+    desscription = models.CharField()
+    prix = models.FloatField()
+    id_fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
+    id_categorie = models.ForeignKey(CategorieProduit, on_delete=models.CASCADE)
+    
