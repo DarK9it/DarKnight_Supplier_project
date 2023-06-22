bed340c0dd49f4c1b56b68c53aa9136df5fae1e0
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-22 10:55:51 +0200

    correction

--- suppliers/models/role_droit.py
+++ suppliers/models/role_droit.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-from activites.models import Droit
-from activites.models import Role
+from suppliers.models import Droit
+from suppliers.models import Role
 
 class RoleDroit(models.Model):
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
--- suppliers/models/user_role.py
+++ suppliers/models/user_role.py
@@ -1,5 +1,5 @@
 from django.db import models
-from activites.models import Role
+from suppliers.models import Role
 from django.contrib.auth.models import User
 
 class UserRole(models.Model):
