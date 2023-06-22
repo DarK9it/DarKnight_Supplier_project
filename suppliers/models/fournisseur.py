05538ac4ed92b5a7259925237d332abd3c10d6f6
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-22 10:51:25 +0200

    correction

--- suppliers/models/fournisseur.py
+++ suppliers/models/fournisseur.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Fournisseur(models.Model):
     
-    nom = models.CharField(max_length=45, null=True)
+    nom_entreprise = models.CharField(max_length=45, null=True)
     adresse = models.CharField(max_length=45, null=True)
     telephone= models.CharField(max_length=45, null=True)
     
