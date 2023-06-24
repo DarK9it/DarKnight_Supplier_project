44837842aa5de90191fe25140359e4fb0514bf72
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-24 08:58:56 +0200

    quelque modifications dans init

--- suppliers/urls.py
+++ suppliers/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from suppliers.views import fournisseurs
+from suppliers.views import fournisseurs, home
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
