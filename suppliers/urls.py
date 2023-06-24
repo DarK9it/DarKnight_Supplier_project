b136354232b4372ca1f229734516c3788caa7e7a
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-24 08:47:25 +0200

    ajout des liens

--- suppliers/urls.py
+++ suppliers/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from suppliers.views import *
+from suppliers.views import fournisseurs
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', home.index, name='home'),
+    path('fournisseurs/', fournisseurs.index, name='fournisseurs_index'),
+    path('fournisseurs/create', fournisseurs.create, name='fournisseurs_create'),
+    path('fournisseurs/store', fournisseurs.store, name='fournisseurs_store'),
+    path('fournisseurs/edit/<int:id>', fournisseurs.edit, name='fournisseurs_edit'),
+    path('fournisseurs/delete/<int:id>', fournisseurs.delete, name='fournisseurs_delete'),
         
 ]
