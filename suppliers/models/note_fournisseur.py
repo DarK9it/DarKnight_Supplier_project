a8c710cdf0437e967fa3ed93344b7e59be7608f5
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-26 09:39:04 +0200

    pycaches

--- suppliers/models/note_fournisseur.py
+++ suppliers/models/note_fournisseur.py
@@ -3,7 +3,7 @@ from suppliers.models import Fournisseur
 
 class NoteFournisseur(models.Model):
     
-    id_fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
+    fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
     note = models.FloatField()
     commentaire = models.CharField(max_length=45, null=True)
  
