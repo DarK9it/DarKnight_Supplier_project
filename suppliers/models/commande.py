4edfd45b17c6cce37d7972bbcc36f06b16149c5f
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-26 16:39:50 +0200

    quelque changements

--- suppliers/models/commande.py
+++ suppliers/models/commande.py
@@ -3,8 +3,9 @@ from suppliers.models import Fournisseur
 
 class Commande(models.Model):
     
+    nom = models.CharField(max_length=45, null=True)
     date_commande = models.DateField()
     fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
     
-    #def __str__(self) -> str:
-        #return self.id
+    def __str__(self) -> str:
+        return self.id
