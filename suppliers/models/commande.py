1e375db43ce5f756bfe9323109d8c93dc7d62300
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-26 16:23:18 +0200

    ajout d'un autre champs

--- suppliers/models/commande.py
+++ suppliers/models/commande.py
@@ -2,7 +2,11 @@ from django.db import models
 from suppliers.models import Fournisseur
 
 class Commande(models.Model):
-    
+
+    nom = models.CharField(max_length=45, null=True)
     date_commande = models.DateField()
     fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.nom
     
