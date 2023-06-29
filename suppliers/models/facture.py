ad2130841df4731ce21b7fa171b56e2756726702
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-29 12:56:54 +0200

    ajout d'autres champs

--- suppliers/models/facture.py
+++ suppliers/models/facture.py
@@ -1,8 +1,11 @@
 from django.db import models
-from suppliers.models import Commande
+from suppliers.models import Commande, Fournisseur
 
 class Facture(models.Model):
     
     commande = models.ForeignKey(Commande, on_delete=models.CASCADE)
+    fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
     date_facture = models.DateField()
     montant_total = models.FloatField()
+    numero_facture = models.IntegerField()
+    
