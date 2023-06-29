6e8e95fd3922e19d6c0fe5e75bd76e9b66295a95
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-29 13:01:02 +0200

    changement sur fournisseur

--- suppliers/models/facture.py
+++ suppliers/models/facture.py
@@ -4,7 +4,7 @@ from suppliers.models import Commande, Fournisseur
 class Facture(models.Model):
     
     commande = models.ForeignKey(Commande, on_delete=models.CASCADE)
-    fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
+    fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE, null=True, default=None)
     date_facture = models.DateField()
     montant_total = models.FloatField()
     numero_facture = models.IntegerField()
