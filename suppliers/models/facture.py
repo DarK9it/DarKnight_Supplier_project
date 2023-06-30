9b6d3ddbecdae0ff734aef42006cdcad5b64d5dc
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-30 13:52:45 +0200

    changement

--- suppliers/models/facture.py
+++ suppliers/models/facture.py
@@ -15,5 +15,5 @@ class Facture(models.Model):
     numero_facture = models.IntegerField()
     statut = models.CharField(max_length=100, choices=STATUT_CHOICES, default='Non Validé')
 
-    def __str__(self):
+    def __str__(self)-> str:
         return self.numero_facture
