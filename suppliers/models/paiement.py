a211a52ea17f5e7131153f5e9c3835881fc6eabc
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-30 14:02:30 +0200

    changement du nom statut

--- suppliers/models/paiement.py
+++ suppliers/models/paiement.py
@@ -13,11 +13,11 @@ class Paiement(models.Model):
     date_paiement = models.DateField()
     mode_paiement = models.CharField(max_length=200, null=True)
     numero_virement = models.CharField(max_length=200, null=True)
-    statut = models.CharField(max_length=100, choices=STATUT_CHOICES, default=None)
+    status = models.CharField(max_length=100, choices=STATUT_CHOICES, default=None)
 
-    #def save(self, *args, **kwargs):
-     #   if self.facture.statut == 'validé':
-      #      self.statut = 'Traité'
-       # else:
-        #    self.statut = 'Non Traité'
-        #super(Paiement, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if self.facture.statut == 'validé':
+            self.statut = 'Traité'
+        else:
+            self.statut = 'Non Traité'
+        super(Paiement, self).save(*args, **kwargs)
