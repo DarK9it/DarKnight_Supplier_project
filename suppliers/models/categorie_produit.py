68dd6945c18240b1ad77f08ef2676b9499d32f16
DarK9it <alainmichelnishimwe14@gmail.com>
2023-06-30 12:58:35 +0200

    changement

--- suppliers/models/categorie_produit.py
+++ suppliers/models/categorie_produit.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class CategorieProduit(models.Model):
     
-    nom_categorie = models.CharField(max_length=45, null=False)
+    nom_categorie = models.CharField(max_length=45, null=True)
     
     def __str__(self) -> str:
         return self.nom_categorie
--- suppliers/models/fournisseur.py
+++ suppliers/models/fournisseur.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Fournisseur(models.Model):
     
-    nom_entreprise = models.CharField(max_length=45, null=False)
+    nom_entreprise = models.CharField(max_length=45, null=True)
     adresse = models.CharField(max_length=45, null=True)
     telephone= models.CharField(max_length=45, null=True)
     email = models.CharField(max_length=45, null=True)
